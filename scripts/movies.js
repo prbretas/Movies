0c9ba6243b89d815ec2a55162a65c8475db452d9
Philippe R. Bretas <86687733+prbretas@users.noreply.github.com>
2023-07-09 20:30:40 -0300

    new update movies
    add new layout iframe

---
+++ scripts/movies.js
@@ -0,0 +1,72 @@
+let linkFilme1 = document.getElementById("linkFilme1");
+let linkFilme2 = document.getElementById("linkFilme2");
+let linkFilme3 = document.getElementById("linkFilme3");
+let linkFilme4 = document.getElementById("linkFilme4");
+let linkFilme5 = document.getElementById("linkFilme5");
+
+let sourceVideos = [];
+sourceVideos[0] = "https://embed.warezcdn.net/filme/tt0317219"; //CARROS
+sourceVideos[1] = "https://embed.warezcdn.net/filme/tt1216475"; //CARROS 2
+sourceVideos[2] = "https://embed.warezcdn.net/filme/tt1630029"; //AVATAR
+sourceVideos[3] = "https://embed.warezcdn.net/filme/tt2911666"; //JOHN WICK DE VOLTA AO JOGO
+sourceVideos[4] = "https://embed.warezcdn.net/filme/tt4425200"; //JOHN WICK UM NOVO DIA PRA MATRR
+sourceVideos[5] = "https://embed.warezcdn.net/filme/tt6146586"; //JW PARABELLUM
+sourceVideos[6] = "https://embed.warezcdn.net/filme/tt10366206"; //BABA YAGA
+sourceVideos[7] = "https://embed.warezcdn.net/filme/tt2906216"; //DUNGEOSN DRAGONS
+sourceVideos[8] = "https://embed.warezcdn.net/filme/tt11145118"; //CREED 3
+sourceVideos[9] = "https://embed.warezcdn.net/filme/tt10151854"; //SHAZAM 2
+sourceVideos[10] = "https://embed.warezcdn.net/filme/tt5433140"; //VELOZES 10
+
+//ALTERANDO O ATRIBUTO SRC DO ELEMENTO HTML VIDEO
+let iframeFilme = document.getElementById("iframeFilme");
+
+function alterarLinkFilme0() {
+  iframeFilme.setAttribute("src", "https://embed.warezcdn.net/filme/tt0317219");
+}
+function alterarLinkFilme1() {
+  iframeFilme.setAttribute("src", "https://embed.warezcdn.net/filme/tt1216475");
+}
+function alterarLinkFilme2() {
+  iframeFilme.setAttribute("src", "https://embed.warezcdn.net/filme/tt1630029");
+}
+function alterarLinkFilme3() {
+  iframeFilme.setAttribute("src", "https://embed.warezcdn.net/filme/tt2911666");
+}
+function alterarLinkFilme4() {
+  iframeFilme.setAttribute("src", "https://embed.warezcdn.net/filme/tt4425200");
+}
+function alterarLinkFilme5() {
+  iframeFilme.setAttribute("src", "https://embed.warezcdn.net/filme/tt6146586");
+}
+function alterarLinkFilme6() {
+  iframeFilme.setAttribute(
+    "src",
+    "https://embed.warezcdn.net/filme/tt10366206"
+  );
+}
+function alterarLinkFilme7() {
+  iframeFilme.setAttribute("src", "https://embed.warezcdn.net/filme/tt2906216");
+}
+
+function alterarLinkFilme8() {
+  iframeFilme.setAttribute(
+    "src",
+    "https://embed.warezcdn.net/filme/tt11145118"
+  );
+}
+
+function alterarLinkFilme9() {
+  iframeFilme.setAttribute(
+    "src",
+    "https://embed.warezcdn.net/filme/tt10151854"
+  );
+}
+
+function alterarLinkFilme10() {
+  iframeFilme.setAttribute(
+    "src",
+    "https://embed.warezcdn.net/filme/tt5433140"
+  );
+}
+
+F;
