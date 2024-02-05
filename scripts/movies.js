9173fa985b20b8e80660ac38983def0d96f477ca
Philippe R. Bretas <phrbretas@gmail.com>
2024-02-04 21:30:05 -0300

    add new feats

--- scripts/movies.js
+++ scripts/movies.js
@@ -5,15 +5,15 @@ let linkFilme4 = document.getElementById("linkFilme4");
 let linkFilme5 = document.getElementById("linkFilme5");
 
 let sourceVideos = [];
-sourceVideos[0] = "https://embedder.net/e/movie?imdb=tt0317219"; //CARROS
-sourceVideos[1] = "https://embed.warezcdn.net/filme/tt1216475"; //CARROS 2
-sourceVideos[2] = "https://embed.warezcdn.net/filme/tt1630029"; //AVATAR
+sourceVideos[0] = "https://embedder.net/e/tt0317219/"; //CARROS
+sourceVideos[1] = "https://embedder.net/e/tt1216475/"; //CARROS 2
+sourceVideos[2] = "https://embedder.net/e/tt1630029/"; //AVATAR 2
 sourceVideos[3] = "https://embedder.net/e/tt2911666/"; //JOHN WICK DE VOLTA AO JOGO
-sourceVideos[4] = "https://embed.warezcdn.net/filme/tt4425200"; //JOHN WICK UM NOVO DIA PRA MATRR
-sourceVideos[5] = "https://embed.warezcdn.net/filme/tt6146586"; //JW PARABELLUM
-sourceVideos[6] = "https://embed.warezcdn.net/filme/tt10366206"; //BABA YAGA
-sourceVideos[7] = "https://embed.warezcdn.net/filme/tt2906216"; //DUNGEOSN DRAGONS
-sourceVideos[8] = "https://embed.warezcdn.net/filme/tt11145118"; //CREED 3
+sourceVideos[4] = "https://embedder.net/e/tt4425200/"; //JOHN WICK UM NOVO DIA PRA MATRR
+sourceVideos[5] = "https://embedder.net/e/tt6146586/"; //JW PARABELLUM
+sourceVideos[6] = "https://embedder.net/e/tt10366206/"; //BABA YAGA
+sourceVideos[7] = "https://embedder.net/e/tt2906216/"; //DUNGEOSN DRAGONS
+sourceVideos[8] = "https://embedder.net/e/tt11145118/"; //CREED 3
 sourceVideos[9] = "https://embed.warezcdn.net/filme/tt10151854"; //SHAZAM 2
 sourceVideos[10] = "https://embedder.net/e/movie?imdb=tt4630562"; //VELOZES 10
 sourceVideos[11] = "https://embedder.net/e/movie?imdb=tt6495056"; //PATOS
@@ -21,6 +21,34 @@ sourceVideos[12] = "https://embedder.net/e/tt15837338/"; //PATRULHA CANINA SUPER
 sourceVideos[13] = "https://embedder.net/e/tt28603694/"; //o porteiro
 sourceVideos[14] = "https://embedder.net/e/tt3291150/"; //MERCENARIOS 4
 sourceVideos[15] = "https://embedder.net/e/tt4495098"; //gran turismo
+
+
+
+
+
+
+
+/*
+https://embedder.net/e/tt3606752/     -   carros 3
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
 
 //ALTERANDO O ATRIBUTO SRC DO ELEMENTO HTML VIDEO
 let iframeFilme = document.getElementById("iframeFilme");
--- scripts/series.js
+++ scripts/series.js
@@ -6,7 +6,7 @@ let linkFilme4 = document.getElementById("linkFilme4")
 let linkFilme5 = document.getElementById("linkFilme5")
 
 let sourceVideos = [];
-sourceVideos[0] = "https://embed.warezcdn.net/serie/tt0898266"; //BIG BANG THEORY
+sourceVideos[0] = "https://embedder.net/e/tt0775431"; //BIG BANG THEORY
 sourceVideos[1] = "https://embed.warezcdn.net/serie/tt7631058"; //HOUSEOFDRAGON
 sourceVideos[2] = "https://embed.warezcdn.net/serie/tt11640018"; //LA BREA
 sourceVideos[3] = "https://embed.warezcdn.net/serie/tt15431326"; //HP CAMP CASAS
@@ -16,38 +16,68 @@ sourceVideos[6] = "https://embed.warezcdn.net/serie/tt7631058"; //LORD OF THE RI
 sourceVideos[7] = "https://embed.warezcdn.net/serie/tt0108778"; //FRIENDS
 
 
+sourceVideos[8] = "https://embedder.net/e/tt6486762/"; //CONTINENTAL DO MUNDO JOHN WICK
+sourceVideos[9] = "https://embedder.net/e/tt1610252/"; //2 AND HALF MAN
+sourceVideos[10] = "https://embed.warezcdn.net/serie/tt0108778"; //FRIENDS
+sourceVideos[11] = "https://embed.warezcdn.net/serie/tt0108778"; //FRIENDS
+sourceVideos[12] = "https://embed.warezcdn.net/serie/tt0108778"; //FRIENDS
+
+
 
 
 //ALTERANDO O ATRIBUTO SRC DO ELEMENTO HTML VIDEO
-let iframeFilme = document.getElementById("iframeFilme")
+let iframeFilme = document.getElementById("iframeFilme");
 
+function alterarLinkFilme0() {
+  iframeFilme.setAttribute("src", sourceVideos[0]);
+}
+function alterarLinkFilme1() {
+  iframeFilme.setAttribute("src", sourceVideos[1]);
+}
+function alterarLinkFilme2() {
+  iframeFilme.setAttribute("src", sourceVideos[2]);
+}
+function alterarLinkFilme3() {
+  iframeFilme.setAttribute("src", sourceVideos[3]);
+}
+function alterarLinkFilme4() {
+  iframeFilme.setAttribute("src", sourceVideos[4]);
+}
+function alterarLinkFilme5() {
+  iframeFilme.setAttribute("src", sourceVideos[5]);
+}
+function alterarLinkFilme6() {
+  iframeFilme.setAttribute("src", sourceVideos[6]);
+}
+function alterarLinkFilme7() {
+  iframeFilme.setAttribute("src", sourceVideos[7]);
+}
 
-function alterarLinkFilme0(){
-    iframeFilme.setAttribute('src', "https://embed.warezcdn.net/serie/tt0898266")
-}
-function alterarLinkFilme1(){
-    iframeFilme.setAttribute('src', "https://embed.warezcdn.net/serie/tt11198330")
-}
-function alterarLinkFilme2(){
-    iframeFilme.setAttribute('src', "https://embed.warezcdn.net/serie/tt11640018")
-}
-function alterarLinkFilme3(){
-    iframeFilme.setAttribute('src', "https://embed.warezcdn.net/serie/tt15431326")
-}
-function alterarLinkFilme4(){
-    iframeFilme.setAttribute('src', "https://embed.warezcdn.net/serie/tt10234724")
-}
-function alterarLinkFilme5(){
-	iframeFilme.setAttribute('src', "https://embed.warezcdn.net/serie/tt11564258")
-}
-function alterarLinkFilme6(){
-    iframeFilme.setAttribute('src', "https://embed.warezcdn.net/serie/tt7631058")
-}
-function alterarLinkFilme7(){
-    iframeFilme.setAttribute('src', "https://embed.warezcdn.net/serie/tt0108778")
+function alterarLinkFilme8() {
+  iframeFilme.setAttribute("src", sourceVideos[8]);
 }
 
+function alterarLinkFilme9() {
+  iframeFilme.setAttribute("src", sourceVideos[9]);
+}
 
+function alterarLinkFilme10() {
+  iframeFilme.setAttribute("src", sourceVideos[10]);
+}
 
+function alterarLinkFilme11() {
+  iframeFilme.setAttribute("src", sourceVideos[11]);
+}
+function alterarLinkFilme12() {
+  iframeFilme.setAttribute("src", sourceVideos[12]);
+}
+function alterarLinkFilme13() {
+  iframeFilme.setAttribute("src", sourceVideos[13]);
+}
+function alterarLinkFilme14() {
+  iframeFilme.setAttribute("src", sourceVideos[14]);
+}
 
-
+function alterarLinkFilme15() {
+  iframeFilme.setAttribute("src", sourceVideos[15]);
+}
