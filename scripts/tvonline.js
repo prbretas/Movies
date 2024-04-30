e98753c7a7b071b3f17825bd59636b458191d47d
Philippe R. Bretas <phrbretas@gmail.com>
2024-04-30 00:25:41 -0300

    add new function and media queries

--- scripts/tvonline.js
+++ scripts/tvonline.js
@@ -10,14 +10,17 @@ sourceVideos[2] = "https://supertvaovivo.co/"; //SUPERTV
 
 function abrirPlutoTv(){
     iframePlutoTv.setAttribute("src", sourceVideos[0]);
+    iframeCanal.setAttribute("src", "");
 }
 
 function selecionarCanal() {
     if (iframeCanal.contentWindow && iframeCanal.contentWindow.document && iframeCanal.contentWindow.document.readyState === 'complete') {
       console.log("A pagina foi carregada com sucesso");
       iframeCanal.setAttribute("src", sourceVideos[1]);
+      iframePlutoTv.setAttribute("src", "");
     } else {
         console.log("A pagina anterior nao foi carregada com sucesso, por isso passou para o proximo link");
       iframeCanal.setAttribute("src", sourceVideos[2]);
+      iframePlutoTv.setAttribute("src", "");
     }
   }
